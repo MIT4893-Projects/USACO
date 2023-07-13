b4ef219b5be534e9d7d14d07c1c400f3d48039ce
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-07-14 06:47:09 +0700

    Add .template.cpp

---
+++ .template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    // Solution
+}
+
+signed main()
+{
+    FAST_IO;
+
+    solve();
+
+    return 0;
+}
